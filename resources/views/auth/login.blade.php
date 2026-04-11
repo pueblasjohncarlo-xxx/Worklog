a418e950cb6d1eb87e221249b1c4c73019bac100
WorkLog Developer <worklog@example.com>
2026-04-12 02:41:46 +0800

    fix: Remove conditional check for registration link visibility on login and welcome pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -79,14 +79,12 @@
             </div>
 
             <div class="text-center mt-4">
-                @if (Route::has('register'))
-                    <p class="text-sm text-gray-600">
-                        {{ __("Don't have an account?") }}
-                        <a href="{{ route('register') }}" class="text-indigo-600 hover:text-indigo-800 font-bold transition-colors ml-1">
-                            {{ __('Create Account') }}
-                        </a>
-                    </p>
-                @endif
+                <p class="text-sm text-gray-600">
+                    {{ __("Don't have an account?") }}
+                    <a href="{{ route('register') }}" class="text-indigo-600 hover:text-indigo-800 font-bold transition-colors ml-1">
+                        {{ __('Create Account') }}
+                    </a>
+                </p>
             </div>
         </form>
     </div>
